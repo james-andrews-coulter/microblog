0a5d342bb5765f8521de00d16d82135603c9fc33
James <hello@jamesandrewscoulter.com>
2025-08-14 14:12:54 +0300

    cleaned up theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Etch';
-export const SITE_DESCRIPTION = 'Astro Etch is a blog style template written in Astro. It is optimized for simplicity.';
+export const SITE_TITLE = "James Coulter's Blog";
+export const SITE_DESCRIPTION = "A daily log of musings";
