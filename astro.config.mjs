91f1c489db3597b29b72e122068303d35d694ea0
james-andrews-coulter <hello@jamesandrewscoulter.com>
2025-08-14 11:18:45 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-etch.pages.dev',
+    site: 'https://blog.james.com',
     integrations: [mdx(), sitemap(), tailwind(), react()],
 });
