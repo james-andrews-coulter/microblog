59a98b89d73257daf512c03953568ecf0a99d839
James <hello@jamesandrewscoulter.com>
2025-08-14 14:47:34 +0300

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "James Coulter's Blog";
-export const SITE_DESCRIPTION = "A daily log of musings";
+export const SITE_TITLE = "Logbook";
+export const SITE_DESCRIPTION = "A feed of daily micro musings";
