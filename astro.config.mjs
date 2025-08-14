ec94a4e2daa0049dc16deed38c510b830246161b
James <hello@jamesandrewscoulter.com>
2025-08-14 11:15:58 +0300

    updated site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-etch.pages.dev',
-    integrations: [mdx(), sitemap(), tailwind(), react()],
+  site: "https://blog.james.com",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
 });
