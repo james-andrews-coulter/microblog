a8a6dbaf3829675b4b4ea574124307649d21c8a6
James <hello@jamesandrewscoulter.com>
2025-08-20 15:28:16 +0300

    checkin object support

--- api/micropub.js
+++ api/micropub.js
@@ -79,7 +79,7 @@ const hasKey = (fm, key) => new RegExp(`(^|\\n)\\s*${key}:`, "i").test(fm);
 
 function inferTypeFromFM(fm) {
   if (hasKey(fm, "bookmark-of")) return "bookmark";
-  if (has("checkin")) return "checkin";
+  if (hasKey(fm, "checkin")) return "checkin";
   if (hasKey(fm, "like-of")) return "like";
   if (hasKey(fm, "repost-of")) return "repost";
   if (hasKey(fm, "in-reply-to")) return "reply";
